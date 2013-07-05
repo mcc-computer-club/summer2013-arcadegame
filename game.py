e388992a863cae7f8b5bf038c154d9f0e6e67643
Douglas Keech <dakeech@gmail.com>
2013-07-04 19:36:25 -0500

    Made movable player that shoots.

--- game.py
+++ game.py
@@ -1,110 +1,59 @@
-# Main game file
-# currently had a test object for the AnimatedSprite class.
+#-------------------------------------------------------------------------------
+# Name:        Main Game File
+# Purpose:     Entry point and main code for the game.
+#
+# Author:      Douglas Keech
+#
+# Created:     16/06/2013
+# Copyright:   (c) MCC Maple Woods Computer Club 2013
+# Licence:     (No licence currently defined.)
+#-------------------------------------------------------------------------------
 
-import pygame, random, time
-
-# Constants
-ARTPATH = 'assets\\art\\'
-
-
-class AnimatedSprite(pygame.sprite.Sprite):
-    x=0
-    y=0
-    images = []
-    frameIndex = 0
-    frameDelay = 0
-    lastFlip = time.time()
-    rect = pygame.Rect(0,0,0,0)
-    def __init__(self, imageFile, frames, frameDelay=.1):
-        pygame.sprite.Sprite.__init__(self)
-        self.frameDelay = frameDelay
-        # Load frames into self.images array of surface objects.
-        for i in range(frames):
-            try:
-                # file name is such: "path\to\file\Xsprite.png" Where x is frame number.
-                img = pygame.image.load(ARTPATH + imageFile + str(i) + ".png").convert_alpha()
-                img.set_colorkey([255, 0, 255])
-            except:
-                #terminal error
-                # Change Error messages after release.
-                print("Someone used the wrong filename, or doesn't have the files.")
-                print("Quitting now, bye.")
-                pygame.quit()
-                quit()
-            self.images.append(img)
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-
-    def flipFrame(self):
-        self.frameIndex += (time.time() - self.lastFlip) // self.framDelay
-        if self.frameIndex > len(self.images)-1:
-            self.frameIndex = 0
-        self.image = self.images[self.frameIndex]
-        # Preserve former position
-        center = self.rect.center
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-        #set the time of the flip under lastFlip
-        self.lastFlip = time.time()
-        
-    def update(self):
-        if time.time() >= self.lastFlip + self.frameDelay:
-            self.flipFrame()
-        
-    def draw(self):
-        screen.blit(self.image, self.rect)
-        
-# Planned class for shots/bullets.
-class Shot(pygame.sprite.Sprite()):
-    def __init__(self, [x,y], shotType=0, flip=False): 
-        pygame.sprite.Sprite.__init__(self)
-        # if.. elif.. else.. block for setting variables dependent on shotType
-        if shotType == 1:
-            imageFile = "regshot" # Regular for now. Something else later
-            self.damage = 20
-        else:
-            imageFile = "regshot" # if shotType ID no. is not available, use regular shots
-            self.damage = 10
-        # load image for sprite
-        self.image = pygame.image.load(ARTPATH + "shot\\" + imageFile + ".png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.center = [x,y]
-        
-# Planned class for ships, including player and enemies
-class Ship(AnimatedSprite):
-    maxHP = int()
-    currHP = int() # Current HP
-    rect = pygame.rect.Rect()
-    shotsArray = pygame.sprite.Group() # An array to hold all subsidiary shot objects
-    def __init__(self, imageFile, maxhp, [x,y]):
-        AnimatedSprite._init_(self)
-        self.maxHP = HP
-        self.hp = self.maxHP
-        self.rect.center = [x,y]
-
-    def shoot(self):
-        
-            
-        
-
-screen = pygame.display.set_mode([800,450])
-clock = pygame.time.Clock()
+import pygame
+import random
+import time
+import gameClasses
 
 
-done = False
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+def main():
+    screen = pygame.display.set_mode([800,450])
+    gameClasses.__init__(screen)
+    clock = pygame.time.Clock()
+    done = False
+    player = gameClasses.Player()
+    # Main Loop
 
-    #update code block
+    while not done:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+                else:
+                    player.eventHandler(event)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    pass
+                else:
+                    player.eventHandler(event)
 
-    
-    # draw code block
-    screen.fill([0,0,0])
-    testAnim.draw()
+        #update code block
+        if done:
+            return # If we're done, why run all the update code?
 
-    pygame.display.flip()
-    clock.tick(60)
+        player.update()
 
+        # draw code block
+        screen.fill([0,0,0]) # Fill with black
+
+        player.draw()
+
+        pygame.display.flip() # Flip make changes visible
+        clock.tick(60) # Limit execution speed.
+
+if __name__ == '__main__':
+#    init()
+    main()
 pygame.quit()
--- starfield_proto.py
+++ starfield_proto.py
@@ -5,9 +5,16 @@
 import pygame, random
 
 invisibleKey = [ 255,   0, 255 ]  # Bright Magenta will be made transparent.
+pygame.init()
 
+# Define screen at 450x800 (700 height is being used for testing
+#   since my screen at least is short)
+screen_width = 500
+screen_height = 450
+screen  = pygame.display.set_mode([screen_width, screen_height])
 
 class Star(pygame.sprite.Sprite):
+    image = pygame.image.load('.\\assets\\art\star_alph.png').convert_alpha()
     xvel = 0
     yvel = 0
     rotVel = 1
@@ -16,18 +23,18 @@ class Star(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         # Load a sprite image in.
-        self.image = pygame.image.load('assets\\art\star.png').convert()
+        self.image = Star.image #pygame.image.load('.\\assets\\art\star.png').convert()#_alpha()
         self.image.set_colorkey(invisibleKey)
         # Get image dimensions into the rect object and set X,Y coords.
         self.rect = self.image.get_rect()
         self.rotAngle = 0
-        
-        
+
+
     def transform(self):
         center = self.rect.center
         self.origWidth = self.image.get_width()
         self.origHeight = self.image.get_height()
-        self.image =  pygame.transform.scale(self.image,
+        self.image =  pygame.transform.scale(Star.image,
                                              [int(self.rect.width*self.factor),
                                               int(self.rect.height*self.factor)])
         self.rect = self.image.get_rect()
@@ -35,33 +42,34 @@ class Star(pygame.sprite.Sprite):
 
     def reset_vars(self):
         # Set X, Y coords and velocity.
-        self.rect.x = random.randrange(screen.get_width())
-        self.rect.y = random.randrange((0-self.rect.height*2),
-                                       (0-self.rect.height*1) , 1)
-        
-        
+        self.rect.y = random.randrange(screen.get_height())
+        self.rect.x = random.randrange(screen_width+(self.rect.width*1),
+                                       screen_width+(self.rect.width*2), 1)
+
+
     def update(self):
-        global screen       #seems easier to extend scope of screen.
+        #global screen       #seems easier to extend scope of screen.
         self.rect.x += self.xvel
         self.rect.y += self.yvel
-        # check if gone off bottom
-        if self.rect.y - self.rect.height > screen.get_height():
+        # check if gone off side
+        if self.rect.x + self.rect.width < 0: #< screen.get_width():
             self.reset_vars()
 
         # rotate
-        #self.rotAngle += self.rotVel
-        #if self.rotAngle > 360-self.rotVel:
-        #    self.rotAngle = 0
-        #self.image = pygame.transform.rotozoom(self.image, self.rotVel, 1)
-            
-        
-pygame.init()
+        center = self.rect.center
+        self.rotAngle += self.rotVel
+        if self.rotAngle > 360-self.rotVel:
+            self.rotAngle = 0
+        self.image = pygame.transform.rotozoom(Star.image, self.rotAngle, self.factor)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
-# Define screen at 450x800 (700 height is being used for testing
-#   since my screen at least is short)
-screen_width = 450
-screen_height = 500
-screen  = pygame.display.set_mode([screen_width, screen_height])
+    def draw(self):
+        screen.blit(self.image, self.rect)
+        #pygame.draw.rect(screen, [255,0,0], self.rect, 1)
+
+
+
 
 # Building the starfield
 starField = pygame.sprite.Group()
@@ -71,15 +79,15 @@ for i in range(100): # Only handle 50 star sprites in starfield
     # Set X, Y coords and velocity.
     star.rect.x = random.randrange(screen_width)
     star.rect.y = random.randrange(screen_height)
-    #star.xvel = random.randrange(-2,2)*10
-    star.xvel = 0
-    star.yvel = random.randrange(1,7)*1
+    star.yvel = 0
+    star.xvel = 0-(random.randrange(1,7)*1)
 
     # Adjust opacity
-    star.image.set_alpha((star.yvel/1)*(255/6))
+    star.image.set_alpha((abs(star.xvel)/1)*(255/6))
 
     # Scaling code
-    star.factor = (1/6)*(star.yvel/1)
+    star.factor = (1/6)*(star.xvel/1)
+    if star.factor < 0: star.factor *= -1
     star.transform()
     starField.add(star)
 
@@ -89,17 +97,25 @@ done = False
 while not done:
 # Events section
     for event in pygame.event.get():
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                done = True
         if event.type == pygame.QUIT:
             done = True
 
 # update section
     starField.update()
 
+
 # draw section
     screen.fill([0,0,0])
-    starField.draw(screen)
+
+    #starField.draw(screen)
+    for star in starField:
+        star.draw()
     pygame.display.flip()
 
-    clock.tick(20)
+
+    clock.tick(30)
 
 pygame.quit()
